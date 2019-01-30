e605244b0a8df2686002565a294284a602a2e0a8
ivanshipitsin <30929400+ivanshipitsin@users.noreply.github.com>
2019-01-30 23:23:17 +0500

    совсем чутка
    чутка добавил

--- Spawnhelper--/src/main/java/io/github/fargothcor/spawnhelper/spawnhelper/Handler.java
+++ Spawnhelper--/src/main/java/io/github/fargothcor/spawnhelper/spawnhelper/Handler.java
@@ -4,10 +4,12 @@ package io.github.fargothcor.spawnhelper.spawnhelper;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-
+import org.bukkit.entity.Player;
 public class Handler implements Listener {
     @EventHandler
     public void join(PlayerJoinEvent e){
-
+        Player p=e.getPlayer();
+        p.chat("Hello bitch");
     }
+    
 }
--- Spawnhelper--/src/main/java/io/github/fargothcor/spawnhelper/spawnhelper/Spawnhelper.java
+++ Spawnhelper--/src/main/java/io/github/fargothcor/spawnhelper/spawnhelper/Spawnhelper.java
@@ -15,6 +15,6 @@ public final class Spawnhelper extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("Ty Ahyel????");
     }
 }
