0f21372517d6551be98f0a71dfb8d024abb0bf31
fargothcor <levchenya.roman@gmail.com>
2019-02-02 12:43:37 +0500

    Spawn fix
    Just fix

--- Spawnhelper--/src/main/java/io/github/fargothcor/spawnhelper/spawnhelper/Spawnhelper.java
+++ Spawnhelper--/src/main/java/io/github/fargothcor/spawnhelper/spawnhelper/Spawnhelper.java
@@ -4,10 +4,17 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender.Spigot;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public final class Spawnhelper extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        File config = new File(getDataFolder() + File.separator +  "config.yml");
+        if (!config.exists()){
+            getConfig().options().copyDefaults(true);
+            saveDefaultConfig();
+        }
         Bukkit.getPluginManager().registerEvents(new Handler(), this);
         getCommand("spawn").setExecutor(new commands(this));
         getLogger().info("PLUGIN WAS STARTED SUCCESSFULLY!!!");
--- Spawnhelper--/src/main/java/io/github/fargothcor/spawnhelper/spawnhelper/commands.java
+++ Spawnhelper--/src/main/java/io/github/fargothcor/spawnhelper/spawnhelper/commands.java
@@ -1,5 +1,6 @@
 package io.github.fargothcor.spawnhelper.spawnhelper;
 
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.CommandExecutor;
@@ -7,6 +8,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import static org.bukkit.Bukkit.getDefaultGameMode;
 import static org.bukkit.Bukkit.getLogger;
 import static org.bukkit.Bukkit.getWorld;
 
@@ -17,18 +19,22 @@ public class commands implements CommandExecutor {
     {
         this.plugin = plugin;
     }
-    
+    public Double x = plugin.getConfig().getDouble("spawn.coords.x"),
+                  y = plugin.getConfig().getDouble("spawn.coords.y"),
+                  z = plugin.getConfig().getDouble("spawn.coords.z");
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
         if (!(sender instanceof Player)) {
             sender.sendMessage("You must be a player!");
             return true;
         }
-        //хуй
-        //more хуй
-        Location loc = new Location(getWorld("world"),225,65,225);
+
+
+        Location loc = new Location(getWorld("world"), x, y, z);
+        ((Player) sender).setGameMode(GameMode.ADVENTURE);
         ((Player) sender).teleport(loc);
-        ((Player) sender).setHealth(20.0);
+        
+        //((Player) sender).setHealth(20.0);
 
         return true;
     }
