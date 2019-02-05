29b6beba21ceeed418817d6a01b1b22c137ec086
ivanshipitsin <30929400+ivanshipitsin@users.noreply.github.com>
2019-02-05 14:56:06 +0500

    Filereader
    1 point

--- Spawnhelper--/src/main/java/io/github/fargothcor/spawnhelper/spawnhelper/Spawnhelper.java
+++ Spawnhelper--/src/main/java/io/github/fargothcor/spawnhelper/spawnhelper/Spawnhelper.java
@@ -1,7 +1,6 @@
 package io.github.fargothcor.spawnhelper.spawnhelper;
 
 import org.bukkit.Bukkit;
-import org.bukkit.command.CommandSender.Spigot;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -10,11 +9,7 @@ public final class Spawnhelper extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        File config = new File(getDataFolder() + File.separator +  "config.yml");
-        if (!config.exists()){
-            getConfig().options().copyDefaults(true);
-            saveDefaultConfig();
-        }
+        this.reloadConfig();
         Bukkit.getPluginManager().registerEvents(new Handler(), this);
         getCommand("spawn").setExecutor(new commands(this));
         getLogger().info("PLUGIN WAS STARTED SUCCESSFULLY!!!");
